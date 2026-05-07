3de89e87fa6b3e10f9e6a7bc1f6d7d4324f5e7c1
pythondavidson <granadossergio662@email.com>
2026-05-07 14:10:51 +0200

    fix: async params en perfil publico

--- src/app/usuario/[username]/page.jsx
+++ src/app/usuario/[username]/page.jsx
@@ -1,1 +1,6 @@
-import PerfilPublico from '../../../components/PerfilPublico'; export default function Page() { return <PerfilPublico />; }
+import PerfilPublico from '../../../components/PerfilPublico';
+
+export default async function Page({ params }) {
+  const { username } = await params;
+  return <PerfilPublico params={{ username }} />;
+}
